c57fe8d0c071e46188481334e79b54a74063b2a6
Arthur Ronconi <arthur.ronconi@gmail.com>
2021-01-27 19:33:26 -0300

    registration feature complete

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
 
 const users = [];
 
@@ -11,13 +13,17 @@ app.get("/users", (req, res) => {
   res.json(users);
 });
 
-app.post("/users", (req, res) => {
+app.post("/users/register", (req, res) => {
   const user = {
     email: req.body.email,
     password: req.body.password,
   };
-  users.push(user);
-  res.json(users);
+  if (users.some((user) => user.email === req.body.email) === false) {
+    users.push(user);
+    res.json({ message: "user added", code: 200 });
+  } else {
+    res.send("user already exists in database");
+  }
 });
 
 const port = 4000;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,8 +10,6 @@ import {
   Checkbox,
 } from "@material-ui/core";
 import { Visibility, VisibilityOff } from "@material-ui/icons";
-import { Link } from "react-router-dom";
-import { theme } from "../global-styles/theme";
 import { useHistory } from "react-router-dom";
 
 // STYLES
@@ -34,10 +32,16 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const Register = () => {
+  // FORM
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmedPassword, setConfirmedPassword] = useState();
+  const [registered, setRegistered] = useState(false);
+
   // CHECKBOX STATE
   const [checked, setChecked] = useState(false);
 
-  // INPUT TYPE STATE
+  // TOGGLE VISIBILITY
   const [inputType, setInputType] = useState("password");
   useEffect(() => {
     if (checked) {
@@ -54,8 +58,25 @@ export const Register = () => {
 
   // ROUTER
   const history = useHistory();
-  const Login = () => {
-    history.push("/in");
+  const Register = async () => {
+    if (password === confirmedPassword) {
+      const response = await fetch("http://localhost:4000/users/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application-json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+      const message = await response.json();
+      if (message.code === 200) {
+        setRegistered(true);
+      } else {
+        setRegistered(false);
+      }
+    }
   };
 
   // STYLES
@@ -85,8 +106,30 @@ export const Register = () => {
             Novo Registro
           </Typography>
         </Grid>
+        {registered && (
+          <Grid item xs={12}>
+            <Typography variant="h6" align="center" color="secondary">
+              Usuário registrado com sucesso
+            </Typography>
+            <Button
+              variant="contained"
+              color="secondary"
+              fullWidth
+              className={classes.button}
+              onClick={() => history.push("/")}
+            >
+              Voltar para login
+            </Button>
+          </Grid>
+        )}
         <Grid item xs={12}>
-          <TextField id="email" label="Email" variant="filled" fullWidth />
+          <TextField
+            id="email"
+            label="Email"
+            variant="filled"
+            fullWidth
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Grid>
         <Grid item xs={12}>
           <TextField
@@ -95,6 +138,7 @@ export const Register = () => {
             variant="filled"
             type={inputType}
             fullWidth
+            onChange={(e) => setPassword(e.target.value)}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
@@ -113,11 +157,12 @@ export const Register = () => {
         </Grid>
         <Grid item xs={12}>
           <TextField
-            id="password"
+            id="confirmPassword"
             label="Confirmar senha"
             variant="filled"
             type="password"
             fullWidth
+            onChange={(e) => setConfirmedPassword(e.target.value)}
           />
         </Grid>
 
@@ -136,7 +181,7 @@ export const Register = () => {
               size="large"
               fullWidth
               className={classes.button}
-              onClick={() => Login()}
+              onClick={() => Register()}
             >
               Registrar-se
             </Button>
